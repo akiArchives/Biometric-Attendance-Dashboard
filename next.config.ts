12af353ce45eca30707b6700cd08ba470f4e89f2
JOAQUIN MIKEL ERMITA <joaquinmikel.ermita@g.msuiit.edu.ph>
2026-06-16 08:25:33 +0800

    Inital commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+module.exports = {
+  allowedDevOrigins: ["192.168.56.1"],
+};
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
